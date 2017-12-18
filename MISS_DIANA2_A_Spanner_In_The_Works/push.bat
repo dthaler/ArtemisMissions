dbee1b0ed7f3688b9422e411010aeaeb7864e833
Dave Thaler <dthaler@ntdev.microsoft.com>
2017-12-17 21:48:24 -0800

    Stubs for "A Spanner in the Works"
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

---
+++ MISS_DIANA2_A_Spanner_In_The_Works/push.bat
@@ -0,0 +1,2 @@
+mkdir C:\Users\dthaler\AppData\Local\Artemis\dat\Missions\MISS_DIANA2_A_Spanner_In_The_Works
+copy MISS_DIANA2_A_Spanner_In_The_Works.xml C:\Users\dthaler\AppData\Local\Artemis\dat\Missions\MISS_DIANA2_A_Spanner_In_The_Works
