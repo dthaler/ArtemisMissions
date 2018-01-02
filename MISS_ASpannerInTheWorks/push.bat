e9e6854b8952fb975609a23a2e24011f2a6f04ee
Dave Thaler <dthaler@microsoft.com>
2018-01-01 16:53:15 -0800

    Rename files
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

---
+++ MISS_ASpannerInTheWorks/push.bat
@@ -0,0 +1,2 @@
+mkdir %LOCALAPPDATA%\Artemis\dat\Missions\MISS_ASpannerInTheWorks
+copy MISS_ASpannerInTheWorks.xml %LOCALAPPDATA%\Artemis\dat\Missions\MISS_ASpannerInTheWorks
--- MISS_DIANA2_A_Spanner_In_The_Works/push.bat
+++
@@ -1,2 +0,0 @@
-mkdir %LOCALAPPDATA%\Artemis\dat\Missions\MISS_DIANA2_ASpannerInTheWorks
-copy MISS_DIANA2_ASpannerInTheWorks.xml %LOCALAPPDATA%\Artemis\dat\Missions\MISS_DIANA2_ASpannerInTheWorks
