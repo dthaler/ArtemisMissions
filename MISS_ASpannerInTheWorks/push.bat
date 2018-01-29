f9bc17f91f1f8397ccfc316d907c4b9303f2bdb4
Dave Thaler <dthaler@microsoft.com>
2018-01-28 20:59:18 -0800

    ADD: support for a second player ship
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- MISS_ASpannerInTheWorks/push.bat
+++ MISS_ASpannerInTheWorks/push.bat
@@ -1,2 +1,3 @@
 mkdir %LOCALAPPDATA%\Artemis\dat\Missions\MISS_ASpannerInTheWorks
-copy MISS_ASpannerInTheWorks.xml %LOCALAPPDATA%\Artemis\dat\Missions\MISS_ASpannerInTheWorks
+copy MISS_ASpannerInTheWorks.xml %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_ASpannerInTheWorks
+copy MISS_ASpannerInTheWorks.xml %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_ASpannerInTheWorks
---
+++ MISS_TheDianaOperation/push.bat
@@ -0,0 +1,3 @@
+mkdir %LOCALAPPDATA%\Artemis\dat\Missions\MISS_TheDianaOperation
+copy MISS_TheDianaOperation.xml %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_TheDianaOperation
+copy MISS_TheDianaOperation.xml %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_TheDianaOperation
