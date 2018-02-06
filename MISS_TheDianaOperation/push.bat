721ba63d0899c37ab62e254fc7b5045a04a25cee
Dave Thaler <dthaler@microsoft.com>
2018-02-05 22:47:28 -0800

    Fix push.bat scripts
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- MISS_TheDianaOperation/push.bat
+++ MISS_TheDianaOperation/push.bat
@@ -1,3 +1,4 @@
-mkdir %LOCALAPPDATA%\Artemis\dat\Missions\MISS_TheDianaOperation
+mkdir %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_TheDianaOperation
+mkdir %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_TheDianaOperation
 copy MISS_TheDianaOperation.xml %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_TheDianaOperation
 copy MISS_TheDianaOperation.xml %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_TheDianaOperation
