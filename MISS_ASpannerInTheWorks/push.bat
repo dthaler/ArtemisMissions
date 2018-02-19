5c6173a9721956e5a29f9f968676af7f0b6c85d6
Dave Thaler <dthaler@microsoft.com>
2018-02-18 22:31:54 -0800

    Work around Artemis v2.7.0 bug by not requiring DamCon team to be full
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- MISS_ASpannerInTheWorks/push.bat
+++ MISS_ASpannerInTheWorks/push.bat
@@ -1,4 +1,6 @@
 mkdir %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_ASpannerInTheWorks
 mkdir %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_ASpannerInTheWorks
+mkdir %LOCALAPPDATA%\Artemis2.7\dat\Missions\MISS_ASpannerInTheWorks
 copy MISS_ASpannerInTheWorks.xml %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_ASpannerInTheWorks
 copy MISS_ASpannerInTheWorks.xml %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_ASpannerInTheWorks
+copy MISS_ASpannerInTheWorks.xml %LOCALAPPDATA%\Artemis2.7\dat\Missions\MISS_ASpannerInTheWorks
