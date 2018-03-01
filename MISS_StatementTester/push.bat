6950191079d34774ebd288b70ab63322571138e1
Dave Thaler <dthaler@ntdev.microsoft.com>
2018-03-01 11:03:00 -0800

    add StatementTester mission
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

---
+++ MISS_StatementTester/push.bat
@@ -0,0 +1,6 @@
+mkdir %LOCALAPPDATA%\Artemis2.7\dat\Missions\MISS_StatementTester
+copy MISS_StatementTester.xml %LOCALAPPDATA%\Artemis2.7\dat\Missions\MISS_StatementTester
+mkdir %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_StatementTester
+copy MISS_StatementTester.xml %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_StatementTester
+mkdir %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_StatementTester
+copy MISS_StatementTester.xml %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_StatementTester
