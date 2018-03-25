815dbeb38d6b661b81949b780dd36853cf91157b
Dave Thaler <dthaler@microsoft.com>
2018-03-24 18:21:09 -0700

    Rename mission to TheDianaOperation2
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- MISS_ASpannerInTheWorks/push.bat
+++
@@ -1,6 +0,0 @@
-mkdir %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_ASpannerInTheWorks
-mkdir %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_ASpannerInTheWorks
-mkdir %LOCALAPPDATA%\Artemis2.7\dat\Missions\MISS_ASpannerInTheWorks
-copy MISS_ASpannerInTheWorks.xml %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_ASpannerInTheWorks
-copy MISS_ASpannerInTheWorks.xml %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_ASpannerInTheWorks
-copy MISS_ASpannerInTheWorks.xml %LOCALAPPDATA%\Artemis2.7\dat\Missions\MISS_ASpannerInTheWorks
---
+++ MISS_TheDianaOperation2/push.bat
@@ -0,0 +1,6 @@
+mkdir %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_TheDianaOperation2
+mkdir %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_TheDianaOperation2
+mkdir %LOCALAPPDATA%\Artemis2.7\dat\Missions\MISS_TheDianaOperation2
+copy MISS_TheDianaOperation2.xml %LOCALAPPDATA%\Artemis2.4\dat\Missions\MISS_TheDianaOperation2
+copy MISS_TheDianaOperation2.xml %LOCALAPPDATA%\Artemis2.6\dat\Missions\MISS_TheDianaOperation2
+copy MISS_TheDianaOperation2.xml %LOCALAPPDATA%\Artemis2.7\dat\Missions\MISS_TheDianaOperation2
