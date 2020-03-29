204e1788a0f23d8b55a79f20d7827d07e54546d4
Dave Thaler <dthaler@ntdev.microsoft.com>
2020-03-28 20:04:05 -0700

    HelmsDeep: Add score variables
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

--- MISS_HelmsDeep/push.bat
+++ MISS_HelmsDeep/push.bat
@@ -1,2 +1,5 @@
 mkdir %LOCALAPPDATA%\Artemis2.7.5\dat\Missions\MISS_HelmsDeep
 copy MISS_HelmsDeep.xml %LOCALAPPDATA%\Artemis2.7.5\dat\Missions\MISS_HelmsDeep
+
+mkdir %LOCALAPPDATA%\Artemis2.7.5stock\dat\Missions\MISS_HelmsDeep
+copy MISS_HelmsDeep.xml %LOCALAPPDATA%\Artemis2.7.5stock\dat\Missions\MISS_HelmsDeep
